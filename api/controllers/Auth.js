5841d1433db9b90b2f8902d415de5f56c800c394
Ciro Chang <cirochang@live.com>
2018-10-06 20:51:23 -0300

    add authenthicate for the backend

---
+++ api/controllers/Auth.js
@@ -0,0 +1,7 @@
+exports.authorize = (req, res, next) => {
+  const password = process.env.API_KEY || 'foobar';
+  const key = req.headers['x-api-key'];
+  if (!key) return res.status(403).send({ success: false, message: 'No token provided.' });
+  if (key !== password) return res.status(401).send({ success: false, message: 'Failed to authenticate token.' });
+  return next();
+};
