3a09a00cf3a31f96212f9ee18c3fd96106fe5392
Ciro Chang <cirochang@live.com>
2018-10-07 21:47:16 -0300

    log exception

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -1,40 +1,51 @@
 const moneeda = require('../gateways/moneeda');
+const logger = require('../utils/logger');
 
 function findProdInEx(product, exchange) {
   return exchange.find(productEx => productEx.id === product.id);
 }
 
 exports.show_all = async (req, res) => {
-  const [responseBNB, responseBTX, responseBFX] = await Promise.all([
-    moneeda.getProducts('BNB'),
-    moneeda.getProducts('BTX'),
-    moneeda.getProducts('BFX'),
-  ]);
-  const productsCommon = responseBNB.data.filter(productBNB =>
-    findProdInEx(productBNB, responseBTX.data) &&
-    findProdInEx(productBNB, responseBFX.data));
-  return res.json(productsCommon);
+  try {
+    const [responseBNB, responseBTX, responseBFX] = await Promise.all([
+      moneeda.getProducts('BNB'),
+      moneeda.getProducts('BTX'),
+      moneeda.getProducts('BFX'),
+    ]);
+    const productsCommon = responseBNB.data.filter(productBNB =>
+      findProdInEx(productBNB, responseBTX.data) &&
+      findProdInEx(productBNB, responseBFX.data));
+    return res.json(productsCommon);
+  } catch (err) {
+    logger.error(err);
+    throw err;
+  }
 };
 
 exports.show_prices = async (req, res) => {
-  const [responseBNB, responseBTX, responseBFX] = await Promise.all([
-    moneeda.getPrices('BNB', req.params.productName),
-    moneeda.getPrices('BTX', req.params.productName),
-    moneeda.getPrices('BFX', req.params.productName),
-  ]);
-  const productPrices = [
-    {
-      exchange: 'BNB',
-      price: responseBNB.data.price,
-    },
-    {
-      exchange: 'BTX',
-      price: responseBTX.data.price,
-    },
-    {
-      exchange: 'BFX',
-      price: responseBFX.data.price,
-    },
-  ];
-  return res.json(productPrices);
+  try {
+    const [responseBNB, responseBTX, responseBFX] = await Promise.all([
+      moneeda.getPrices('BNB', req.params.productName),
+      moneeda.getPrices('BTX', req.params.productName),
+      moneeda.getPrices('BFX', req.params.productName),
+    ]);
+    const productPrices = [
+      {
+        exchange: 'BNB',
+        price: responseBNB.data.price,
+      },
+      {
+        exchange: 'BTX',
+        price: responseBTX.data.price,
+      },
+      {
+        exchange: 'BFX',
+        price: responseBFX.data.price,
+      },
+    ];
+    return res.json(productPrices);
+  } catch (err) {
+    logger.error(err);
+    throw err;
+  }
 };
